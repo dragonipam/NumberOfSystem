2db5fcf51a07c6f69d22b690284988f45d7c707f
Kulakov D.A., ISTb-21-1 <dragonipam@yandex.ru>
2022-10-03 08:59:36 +0800

    add class Color and method Print

---
+++ Color.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSV_Color__LAB3
+{
+    public class Color
+    {
+        private int HUE;
+        private byte saturation;
+        private byte value;
+        public Color(int HUE, byte saturation, byte value)
+        {
+            this.HUE=HUE;
+            this.saturation = saturation;
+            this.value=value;
+        }
+        //x is >= 0 and <= 100
+        public string Print()
+        {
+            return String.Format("{0} {1} {2}", this.HUE, this.saturation, this.value);
+        }
+    }
+}
